743f94a0ea52acecc0b14e8f96723af1dab1f9a7
NotSoClassy <73051771+NotSoClassy@users.noreply.github.com>
2020-10-31 17:02:23 -0400

    testing embeds

--- tests/main.lua
+++ tests/main.lua
@@ -4,10 +4,24 @@ local client = toast.Client {
     prefix = {'!', '?', 'space '}
 }
 local ping = toast.Command('ping')
+
 ping.execute = function(msg, args)
     msg:reply('Pong!')
 end
 ping.aliases = {'pong', 'pping'}
+
 client:addCommand(ping)
+
+local embed = toast.Command('embed')
+
+embed.execute = function(msg, args)
+    local embed = toast.Embed()
+        :setColor('random')
+        :addField('NAME', 'VALUE', true)
+        :setTitle('TITLE')
+        :setDescription(string.rep('a', 2050))
+    print(msg:reply(embed))
+end
+client:addCommand(embed)
 
 client:login(config.token)
