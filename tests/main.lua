e8fe7685de4ed4c31f005016bf5da1a0468a4f1b
NotSoClassy <73051771+NotSoClassy@users.noreply.github.com>
2020-10-31 04:46:58 -0400

    Update main.lua

--- tests/main.lua
+++ tests/main.lua
@@ -1,7 +1,14 @@
 local config = require('./config')
 local toast = require('../init')
 local client = toast.Client{
-    prefix = '!'
+    prefix = 'test '
+}
+
+client:addCommand{
+    name = 'ping',
+    execute = function(msg, args)
+        msg:reply('Pong!')
+    end
 }
 
 client:login(config.token)
