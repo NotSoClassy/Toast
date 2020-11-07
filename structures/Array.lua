1de3a79616198b9e0a0cdbba8f2851647e63f189
NotSoClassy <73051771+NotSoClassy@users.noreply.github.com>
2020-11-07 01:35:12 -0500

    It works!

--- structures/Array.lua
+++ structures/Array.lua
@@ -1,35 +1,27 @@
-local discordia = require('discordia')
+local meta = {}
 
-local class = discordia.class
-
-local Array = class('Array')
-
-function Array:__init(...)
-    self._array = {...}
-end
-
-function Array:fill(value, pos1, pos2)
+function meta:fill(value, pos1, pos2)
     if not pos1 then pos1 = 0 end
     if not pos2 then pos2 = #self._array end
-    if pos1 < #self._array then pos1 = #self._array end -- If pos1 is greater than array length it wont be an array anymore so yea
+    if pos1 < #self._array then pos1 = #self._array end
     for i = pos1, pos2 do
         table.insert(self._array, i, value)
     end
     return self
 end
 
-function Array:pop()
+function meta:pop()
     return table.remove(self._array, #self._array)
 end
 
-function Array:push(...)
+function meta:push(...)
     for _, v in ipairs({...}) do
         table.insert(self._array, v)
     end
     return #self._array
 end
 
-function Array:reverse()
+function meta:reverse()
     local reversed = {}
     for _, v in ipairs(self._array) do
         table.insert(self._array, 1, v)
@@ -37,8 +29,22 @@ function Array:reverse()
     return reversed
 end
 
-function Array:shift()
+function meta:shift()
     return table.remove(self._array, 1)
 end
 
-return Array
+return setmetatable({_array = {}}, {
+    __call = function(self, ...)
+        setmetatable(self, {
+            __index = function(self, index)
+                return self._array[index] or meta[index]
+            end,
+            __newindex = function(self, index, value)
+                if #self._array < index then return end
+                self._array[index] = value
+            end
+        })
+        self:push(...)
+        return self
+    end,
+})
