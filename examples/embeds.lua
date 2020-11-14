4196df33588614b6a65daa4e5ca613d3373f459e
canada <NotSoClassy357@gmail.com>
2020-11-14 03:14:28 -0500

    Update embeds.lua

--- examples/embeds.lua
+++ examples/embeds.lua
@@ -1,17 +1,16 @@
 local toast = require('toast')
 local client = toast.Client()
 
-local command = toast.Command('embed')
-
-command.aliases = {'embed'}
-command.execute = function(msg)
-    local embed = toast.Embed()
-        :setTitle('title')
-        :setDescription('description')
-        :addField('name', 'value', true)
-        :setColor('Random')
-    embed:send(msg.channel)
-end
+local command = toast.Command('embed', {
+    execute = function(msg)
+        local embed = toast.Embed()
+            :setTitle('title')
+            :setDescription('description')
+            :addField('name', 'value', true)
+            :setColor('Random')
+        embed:send(msg.channel)
+    end
+})
 
 client:addCommand(command)
 
