7bde55bf9d649e4dc02879760b9cd3b962e588f3
NotSoClassy <NotSoClassy357@gmail.com>
2020-11-15 02:05:17 -0500

    updated version

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
   return {
     name = "NotSoClassy/Toast",
-    version = "1.0.0",
+    version = "1.0.1",
     description = "A Framework for Discordia",
     tags = { "discordia", "discord" },
     license = "MIT",
