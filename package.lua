cc7708ce7b3ff9acf56b4aae728b0fad5cbde903
NotSoClassy <NotSoClassy357@gmail.com>
2020-11-15 01:22:29 -0500

    Update package.lua

--- package.lua
+++ package.lua
@@ -1,11 +1,16 @@
-return {
-   name = 'Toast',
-   version = '1.0.0',
-   description = 'Framework for Discordia',
-   tags = {'discordia', 'discord'},
-   license = 'MIT',
-   author = {name = 'NotSoClassy', email = 'notsoclassy357@gmail.com'},
-   homepage = 'https://github.com/NotSoClassy/Toast',
-   dependencies = {},
-   files = {'**.lua', '!test*'}
-}
+  return {
+    name = "NotSoClassy/Toast",
+    version = "1.0.0",
+    description = "A Framework for Discordia",
+    tags = { "discordia", "discord" },
+    license = "MIT",
+    author = { name = "NotSoClassy", email = "NotSoClassy357@gmail.com" },
+    homepage = "https://github.com/NotSoClassy/Toast",
+    dependencies = {
+      "SinisterRectus/discordia@2.8.4"
+    },
+    files = {
+      "**.lua",
+      "!test*"
+    }
+  }
