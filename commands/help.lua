cb8bf8f998c7c7fb10178d1b66df6000b94f3126
NotSoClassy <NotSoClassy357@gmail.com>
2020-11-19 07:51:50 -0500

    Generate embed here instead of in the command

--- commands/help.lua
+++ commands/help.lua
@@ -1,8 +1,22 @@
 local toast = require('../init')
 
+local function embedGen(self)
+    local aliases = table.concat(self._aliases, '\n')
+    local perms = table.concat(self._userPerms, '\n')
+    return toast.Embed()
+       :setColor('random')
+       :setTitle(self._name:gsub('^(.)', string.upper))
+       :setDescription(self._description)
+       :addField('Usage:', self._example)
+       :addField('Aliases:', #aliases == 0 and 'None' or aliases)
+       :addField('Perms:', #perms == 0 and 'None' or perms)
+       :setFooter('This command has a ' .. math.floor(self._cooldown / 1000)  .. ' second cooldown')
+ end
+
 local function search(tbl, q, where)
+    if not tbl then return end
     for _, v in pairs(tbl) do
-        if v[where] == q or search(v.aliases or {}, v, 'aliases') then
+        if v[where] == q or search(v.aliases, v, 'aliases') then
             return v
         end
     end
@@ -19,7 +33,7 @@ return toast.Command('help', {
 
             if not command then return msg:reply('No command or alias found for `' .. query .. '`') end
 
-            return msg:reply(command.helpEmbed)
+            return msg:reply(embedGen(command))
         else
             local description = ''
 
