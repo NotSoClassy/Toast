627e5d5be9bc6b9e7698f11354a423332fe0a951
NotSoClassy <NotSoClassy357@gmail.com>
2020-11-20 07:52:29 -0500

    fixed bug where you cant get help with the alias name

--- commands/help.lua
+++ commands/help.lua
@@ -1,7 +1,7 @@
 local toast = require('../init')
 
 local function embedGen(self)
-    local aliases = table.concat(self._aliases, ', ')
+    local aliases = table.concat(self._aliases, '\n')
     local perms = table.concat(self._userPerms, ', ')
     local other = self._nsfw and 'NSFW only'
     return toast.Embed()
@@ -10,15 +10,15 @@ local function embedGen(self)
        :setDescription(self._description)
        :addField('Usage:', self._example, true)
        :addField('Aliases:', #aliases == 0 and 'None' or aliases, true)
-       :addField('Perms:', #perms == 0 and 'None' or perms, true)
+       :addField('Permissions:', #perms == 0 and 'None' or perms, true)
        :addField('Other:', other and other or 'None', true)
        :setFooter(self._cooldown ~= 0 and 'This command has a ' .. math.floor(self._cooldown / 1000)  .. ' second cooldown' or 'This command has no cooldown')
  end
 
-local function search(tbl, q, where)
-    if not tbl then return end
-    for _, v in pairs(tbl) do
-        if v[where] == q or search(v.aliases, v, 'aliases') then
+local function findCommand(cmds, q)
+    if not cmds then return end
+    for _, v in pairs(cmds) do
+        if v.name == q or v == q or findCommand(v.aliases, q) then
             return v
         end
     end
@@ -31,7 +31,7 @@ return toast.Command('help', {
         local query = table.concat(args, ' ')
 
         if query and #query ~= 0 then
-            local command = search(msg.client.commands, query, 'name')
+            local command = findCommand(msg.client.commands, query)
 
             if not command then return msg:reply('No command or alias found for `' .. query .. '`') end
 
