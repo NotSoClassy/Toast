2ac5d3cd1b99b963e1081c9d4a1b4ed2341e8a1b
NotSoClassy <NotSoClassy357@gmail.com>
2020-12-22 22:00:07 -0500

    put this here because why not

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ assert(success, 'Toast requires Discordia to function\n' .. tostring(err))
 return {
    package = require('./package'),
    util = require('./util'),
+   argparser = require('./argparser'),
    Client = require('./structures/Client'),
    Embed = require('./structures/Embed'),
    Command = require('./structures/Command')
