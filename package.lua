89114d8db634d151ecf65644b4af855d8209b6b4
canada <notsoclassy357@gmail.com>
2021-01-28 12:15:54 -0500

    bump

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
     name = 'NotSoClassy/toast',
-    version = '1.3.3',
+    version = '1.3.4',
     description = 'A Framework for Discordia',
     tags = {'discordia', 'discord'},
     license = 'MIT',
