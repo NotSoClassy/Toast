a4d673bf1eb3590b43560cef01bc396fde66ee0e
canada <notsoclassy357@gmail.com>
2021-03-12 16:36:59 -0500

    fixed bug where flags/args wouldn't be parsed

--- package.lua
+++ package.lua
@@ -1,11 +1,11 @@
 return {
     name = 'NotSoClassy/toast',
-    version = '1.4.6',
+    version = '1.4.7',
     description = 'A Framework for Discordia',
     tags = {'discordia', 'discord'},
     license = 'MIT',
     author = {name = 'NotSoClassy', email = 'NotSoClassy357@gmail.com'},
     homepage = 'https://github.com/NotSoClassy/Toast',
     dependencies = {'SinisterRectus/discordia@2.8.4'},
-    files = {'**.lua', '!test*', '!spec*'}
+    files = {'**.lua', '!test*', '!spec*', '!runner'}
 }
